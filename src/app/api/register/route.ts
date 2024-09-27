93a04869d35e6c66d331e4841b79355ca0c56819
Draimonox <Draimonox@gmail.com>
2024-09-27 04:28:09 -0400

    blogging app

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        {
+          error: `Please do not leave any fields empty`,
+        },
+        { status: 400 }
+      );
+    }
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
+    const createUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hash,
+      },
+    });
+    return NextResponse.json(createUser, { status: 201 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal Server Error", details: (err as Error).message },
+      { status: 500 }
+    );
+  }
+}
