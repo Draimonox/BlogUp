d2dca5cbab5b1952d09ee8e5a9e55bae6cef59d1
Draimonox <Draimonox@gmail.com>
2024-09-29 03:16:48 -0400

    Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { Center } from "@mantine/core";
+
+function Login() {
+  return (
+    <>
+      <Center h={600}>
+        <h1>Hellow World </h1>
+      </Center>
+    </>
+  );
+}
+
+export default Login;
